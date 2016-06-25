8d4938cd487c6833d7166e55a6908f5f5da891f1
smanley83 <mika17700@hotmail.fr>
2016-06-25 12:58:51 +0200

    Ajout classes (vides)S,Z et package GUI

---
+++ src/tetris/pieces/Z.java
@@ -0,0 +1,20 @@
+package tetris.pieces;
+
+import tetris.Grid;
+import tetris.Piece;
+
+public class Z extends Piece {
+
+	@Override
+	public void rotationR(Grid grid) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rotationL(Grid grid) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tetrisGUI/GameGUI.java
@@ -0,0 +1,5 @@
+package tetrisGUI;
+
+public class GameGUI {
+
+}
